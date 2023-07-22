99f9c099270cf336589b637445186624e48fe0e0
gosha <gosha.artemov@gmail.com>
2023-07-23 01:15:11 +0400

    First commit

---
+++ src/main/java/ru/skypro/lessons/springboot/cursovaya3/Cursovaya3Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.cursovaya3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursovaya3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursovaya3Application.class, args);
+	}
+
+}
